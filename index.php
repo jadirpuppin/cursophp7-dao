7e6b5626616a33c01d34b3f5b806f4ad033f0372
Jadir Puppin <jadirpuppin@gmail.com>
2019-04-09 14:46:20 -0300

    metodos listar usuario

--- index.php
+++ index.php
@@ -2,9 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-$root->loadById(3);
+// CArrega um usuário
+// $root = new Usuario();
+// $root->loadById(3);
+// echo $root;
 
-echo $root;
+// Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+// CArrega lista de usuario buscado pelo login
+// $search = Usuario::search("ja");
+// echo json_encode($search);
+
+// Carrega usuario autenticado
+$usuario = new Usuario();
+$usuario->login("jadir", "1234567");
+echo $usuario;
 
 ?>
